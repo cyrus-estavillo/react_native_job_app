5922c4d80aeb76fb19b79f8336cd57c6427fa54f
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-06 18:19:29 -0500

    Creating unique page for jobs. Incomplete

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style.js'
-// import { checkImageURL } from '../../../utils'
+import { checkImageURL } from '../../../../utils'
+// ended up not having to use utils file because it doesn't work properly
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
 
@@ -21,6 +22,13 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         />
       </TouchableOpacity>
       <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+      <View style={styles.infoContainer}>
+          <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+            {item.job_title}
+          </Text>
+          <Text style={styles.location}>{item.job_country}</Text>
+      </View>
 
     </TouchableOpacity>
   )
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -19,6 +19,11 @@ const Popularjobs = () => {
       num_pages: "1",
     })
 
+  const [selectedJob, setSelectedJob] = useState()
+  const handleCardPress = (item) = {
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
